89b071e990676ae3232bbb4aceb5d81f03cdaeeb
Spencer Lefever <slefever1@gulls.salisbury.edu>
2023-12-27 22:13:02 -0500

    Continue kotlin conversion and start to add nav graphs

---
+++ TodoList/features/home/src/main/java/com/example/home/navigation/HomeFragmentRouter.kt
@@ -0,0 +1,7 @@
+package com.example.home.navigation
+
+import androidx.navigation.NavController
+
+interface HomeFragmentRouter {
+    fun show(navController: NavController)
+}
---
+++ TodoList/features/home/src/main/java/com/example/home/navigation/HomeFragmentRouterImpl.kt
@@ -0,0 +1,13 @@
+package com.example.home.navigation
+
+import androidx.navigation.NavController
+import javax.inject.Inject
+
+class HomeFragmentRouterImpl @Inject constructor() : HomeFragmentRouter {
+
+    override fun show(navController: NavController) {
+        navController.navigate(
+
+        )
+    }
+}
